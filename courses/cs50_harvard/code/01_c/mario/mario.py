42349dbb9afe4010bf331c049be6355cbd3f1404
Manuel Reyes <manuelreyesv410@gmail.com>
2026-02-03 14:17:13 -0500

    python: update script by including comments to improve readability and reference in the future

--- courses/cs50_harvard/code/01_c/mario/mario.py
+++ courses/cs50_harvard/code/01_c/mario/mario.py
@@ -4,22 +4,24 @@ while True:
     height = input("Enter height between 1 and 8: ")
     if height.isdigit():
         height = int(height)
-        if height > 1 and height < 8:
+        if height >= 1 and height < 8:
             break
     else:
         height = 0
-        
+
+# Print a pyramid for that height        
 for i in range(height):
-        
+    
+    # Inner Loop 1: Print the spaces
     for j in range(height - i - 1):
         print(" ", end="")
-        
+    # Inner loop 2: Print left hashes
     for k in range(i + 1):
         print("#", end="")
-        
+    # THE GAP: Print 2 spaces (no loop needed, it's constant)    
     print("  ", end="")
-        
+    # Inner loop 3: Print right hashes    
     for l in range(i + 1):
         print("#", end="")
             
-    print("\n", end="")
+    print("\n", end="") 
