f794259b5fe18cd256c4ebd233bb90043add08aa
Manuel Reyes <manuelreyesv410@gmail.com>
2026-05-16 21:27:45 -0400

    chore(documentation): add inline comments to explain the code: class-level attributes, instance-level attributes, special methods used for the new built object and script purpose, to improve code readability and for future reference.

--- books/fluent_python/data_structures/the_python_data_model/card_deck.py
+++ books/fluent_python/data_structures/the_python_data_model/card_deck.py
@@ -1,24 +1,32 @@
+# Practice special methods in Python classes (dunder = double underscore function)
+
 from __future__ import annotations
 
 import collections
 
+# Construct a simple class to represent individual cards
 Card = collections.namedtuple("Card", ["rank", "suit"])
 
 class FrenchDeck:
+    # Class-level attributes -> shared to all instances
     ranks = [str(n) for n in range(2, 11)] + list("JQKA")
     suits = "spades diamonds clubs hearts".split()
     
     def __init__(self) -> None:
+        # Instance-level attributes -> each per instance
         self._cards = [
             Card(rank, suit)
             for suit in self.suits
             for rank in self.ranks
         ]
-        
+
+    # Get size of object (instance)
     def __len__(self) -> int:
         return len(self._cards)
     
-    def __getitem__(self, position) -> Card:
+    # Allow object to be called by:
+    #   x in y, y[:], reversed(), sorted(), etc...
+    def __getitem__(self, position: int) -> Card:
         return self._cards[position]
     
 
