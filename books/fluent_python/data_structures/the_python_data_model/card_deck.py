b95753a396b6e9dd31827edc202dfb30496d47fc
manuel-reyes-ml <manuelreyesv410@gmail.com>
2026-09-14 09:52:23 -0400

    style: apply pre-commit fixes across scripts and workspace members

--- books/fluent_python/data_structures/the_python_data_model/card_deck.py
+++ books/fluent_python/data_structures/the_python_data_model/card_deck.py
@@ -11,7 +11,7 @@ class FrenchDeck:
     # Class-level attributes -> shared to all instances
     ranks = [str(n) for n in range(2, 11)] + list("JQKA")
     suits = "spades diamonds clubs hearts".split()
-    
+
     def __init__(self) -> None:
         # Instance-level attributes -> each per instance
         self._cards = [
@@ -23,12 +23,12 @@ class FrenchDeck:
     # Get size of object (instance)
     def __len__(self) -> int:
         return len(self._cards)
-    
+
     # Allow object to be called by:
     #   x in y, y[:], reversed(), sorted(), etc...
     def __getitem__(self, position: int) -> Card:
         return self._cards[position]
-    
+
 
 deck = FrenchDeck()
 
--- books/fluent_python/data_structures/the_python_data_model/vector_ops.py
+++ books/fluent_python/data_structures/the_python_data_model/vector_ops.py
@@ -6,31 +6,31 @@ import math
 
 
 class Vector:
-    
+
     def __init__(self, x: int = 0, y: int = 0) -> None:
         # Instance-level attributes
         self.x = x
         self.y = y
-        
+
     def __repr__(self) -> str:
         return f"Vector({self.x!r}, {self.y!r})"
-    
+
     def __abs__(self) -> float:
         return math.hypot(self.x, self.y)
-    
+
     def __bool__(self) -> bool:
         return bool(abs(self))
-    
+
     # Method creates and returns a new instance of Vector and
     # does not modify self.
     def __add__(self, other: Vector) -> Vector:
         x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
-    
+
     def __mul__(self, scalar: int) -> Vector:
         return Vector(self.x * scalar, self.y * scalar)
-    
+
 v1 = Vector(1, 1)
 v2 = Vector(2, 2)
 
